ee89324c2cbbf19a803fd4a5a7f9150e0c369735
Manuel Fahmy <manuelfahmy@gmail.com>
2020-05-13 14:53:51 +0200

    Python test

---
+++ test.py
@@ -0,0 +1,12 @@
+print("Hello World!");
+
+
+FirstNumber = 2;
+SecondNumber = 2; 
+
+print(FirstNumber+SecondNumber);
+
+if (FirstNumber==1):
+    print("First Number is 1!");
+else:
+    print("First Number is not 1!");
