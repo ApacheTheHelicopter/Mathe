bbb792d9e9757f94cc7c03eb9b638075efe2aed2
Manuel Fahmy <manuelfahmy@gmail.com>
2020-05-13 15:39:49 +0200

    if/else statements; input tests and numbers

--- test.py
+++ test.py
@@ -11,4 +11,23 @@ if (FirstNumber==1):
 else:
     print("First Number is not 1!");
 
-    
+#Name program and Input test
+print("Enter your name: ");
+nameinput = input()
+print("Hello, " + nameinput + ". Now tell me your age, please: ");
+
+#Age program and input
+age = float(input())
+
+#True=adult; False=minor
+ageCheck = age >= 18;
+
+if (ageCheck == True):
+    ageref = "an Adult";
+else:
+    ageref = "a minor";
+
+print("Oh, so you´re " + ageref);
+
+
+
