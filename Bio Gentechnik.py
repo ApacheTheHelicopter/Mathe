8fca73153596578de56bf3e3f0df3ca2b83b14f0
Manuel Laptop <manuelfahmyz@gmail.com>
2020-05-15 12:24:20 +0200

    Bio update

---
+++ Bio Gentechnik.py
@@ -0,0 +1,26 @@
+# Genvariationen mit Generationen ausrechnen
+
+# Parentalgeneration
+
+# Strings nehmen und miteinander randomizen?
+# So, dass man dann alle Kombinationen hat
+# Übelst unnötig eigentlich
+
+# -- 1. Objekt
+
+    # 1. Allel
+allel1 = "G"
+
+    # 2. Allel
+allel2 = "g"
+
+# -- 2. Objekt
+
+    # 3. Allel
+allel3 = "G"
+
+    # 4. Allel
+allel4 = "g"
+
+# randomizer
+
