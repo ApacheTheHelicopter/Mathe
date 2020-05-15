c47c299d8c33bf763a197cbc14af9f850ad6ed69
Manuel Laptop <manuelfahmyz@gmail.com>
2020-05-15 11:11:36 +0200

    new file

--- TempMatheZeug.py
+++ TempMatheZeug.py
@@ -1,1 +1,30 @@
 print("Hallo")
+
+print("Input number 1:")
+number1 = input()
+print("Input number 2:")
+number2 = input()
+
+print()
+
+#add
+add = str(float(number1)+float(number2))
+print(number1 + " + " + number2 + " = " + add)
+
+print()
+
+#substract
+substract = str(float(number1)-float(number2))
+print(number1 + " - " + number2 + " = " + substract)
+
+print()
+
+#multiply
+multiply = str(float(number1)*float(number2))
+print(number1 + " * " + number2 + " = " + multiply)
+
+print()
+
+#divide
+divide = str(float(number1)/float(number2))
+print(number1 + " / " + number2 + " = " + divide)
