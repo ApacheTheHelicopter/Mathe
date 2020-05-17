ffd1a61b4abef4e4b93e1409602b4bceb4aa736b
Manuel Fahmy <manuelfahmy@gmail.com>
2020-05-17 03:02:21 +0200

    rename

--- Simple Ai test using numbers and facts.py
+++ Simple Ai test using numbers and facts.py
@@ -8,8 +8,8 @@ model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
 #data
-xs=[1, 2, 3]
-ys=[2, 4, 6]
+xs=[1, 2, 3, 4, 5, 6]
+ys=[2, 4, 6, 8, 10, 12]
 
 #Train (Epochs gibts Durchläufe an)
 model.fit(xs, ys, epochs=1000)
