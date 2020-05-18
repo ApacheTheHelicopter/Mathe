6c7ea0d50026f4cfa8a3b6ad6a0386a53e1aa715
Manuel Fahmy <manuelfahmy@gmail.com>
2020-05-18 16:03:28 +0200

    update

--- Machine Learning/Strings in neural networks.py
+++ Machine Learning/Strings in neural networks.py
@@ -1,3 +1,13 @@
 import tensorflow as tf 
 from tensorflow import keras
 
+model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+FirstStringArray = ["A", "B", "C", "D", "E", "F"]
+SecondStringArray = [1, 2, 3, 4, 5, 6]
+
+model.fit(FirstStringArray, SecondStringArray, epochs=1000)
+
+print(model.predict(["G"]))
