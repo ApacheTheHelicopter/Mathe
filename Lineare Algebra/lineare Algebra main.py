0d42f736a459ac67ba4068945a1a570f35a63884
Manuel Laptop <manuelfahmyz@gmail.com>
2020-05-19 10:25:29 +0200

    updates and new bugs

---
+++ Lineare Algebra/lineare Algebra main.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+# Vector build 
+x = np.linspace(4, 2, 3)
+
+
+
+
+# Vektoren aus Punkte erstellen
+
+def makeVector(a, b):
+    c = b-c
+    return c
+
+
+
+# plot
+
+N = 3
+y = np.zeros(N)
+x1 = np.linspace(0, 10, N, endpoint=True)
+x2 = np.linspace(0, 10, N, endpoint=False)
+
+plt.plot(x, y, "o")
+
+plt.show()
