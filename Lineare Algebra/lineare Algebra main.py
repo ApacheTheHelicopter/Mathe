af7d8488909a4b8e1b4f6fe75678591e3f432c55
Manuel Laptop <manuelfahmyz@gmail.com>
2020-05-19 14:55:02 +0200

    updates

--- Lineare Algebra/lineare Algebra main.py
+++ Lineare Algebra/lineare Algebra main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
+"""
 # Vector build 
 x = np.linspace(4, 2, 3)
 
@@ -24,6 +24,65 @@ y = np.zeros(N)
 x1 = np.linspace(0, 10, N, endpoint=True)
 x2 = np.linspace(0, 10, N, endpoint=False)
 
-plt.plot(x, y, "o")
+#plt.plot(x, y, "o")
 
-plt.show()
+#plt.show()
+"""
+
+# vector add
+# wird mit arrays gemacht
+
+print("Choose two Vectors. Default value is 1 ")
+
+print("First: ")
+
+Vektor1 = np.array([])
+n = int(input("Enter the number of values you want:  "))
+
+for i in range(n):
+    v = input("Element:  ")
+    Vektor1 = np.append(Vektor1, v)
+print(Vektor1)
+
+print("")
+print("Now the second vector: ")
+
+Vektor2 = np.array([])
+n = int(input("Enter the number of values you want:  "))
+
+for i in range(n):
+    v = input("Element:  ")
+    Vektor2 = np.append(Vektor2, v)
+print(Vektor2)
+
+
+def VectorAdd(a, b):
+    str(c = a + b) 
+    return "c"
+
+def VektorSubs(a, b): 
+    a = Vektor1
+    b = Vektor2
+    c = a - b
+    return c
+
+def VektorMulti(a, b): 
+    a = Vektor1
+    b = Vektor2
+    c = a * b 
+    return c
+
+def VektorDivd(a, b): 
+    a = Vektor1
+    b = Vektor2
+    c = a / b
+    return c
+
+print("Choose what to do with the Vector!")
+print("You can choose from 'add', 'sub', 'mult' and 'div'")
+
+choose = input()
+
+if (choose == "add"):
+    print(VectorAdd(Vektor1, Vektor2))
+    
