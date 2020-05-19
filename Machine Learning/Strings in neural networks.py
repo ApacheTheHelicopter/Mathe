b4cb2d334ad463286fd530beaf4865ae22c68daf
Manuel Laptop <manuelfahmyz@gmail.com>
2020-05-19 09:23:07 +0200

    introducing some more bugs, nice

--- Machine Learning/Strings in neural networks.py
+++ Machine Learning/Strings in neural networks.py
@@ -1,6 +1,5 @@
 import tensorflow as tf 
 from tensorflow import keras
-import numpy as np
 
 # neurons
 model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
@@ -10,11 +9,11 @@ model.compile(optimizer='sgd', loss='mean_squared_error')
 
 
 # data
-FirstStringArray = ["1", "2", "3"]
-SecondList = ["1", "2", "3"]
+FirstStringArray = [1, 2, 3, 7, 10, 20, 21]
+SecondList = [1, 2, 3, 7, 10, 20, 21]
 
 
 model.fit(FirstStringArray,SecondList, epochs=1000)
 
 
-print(model.predict(["2"]))
+print(model.predict([30]))
