8d4166d1258ad21b328e592aa04528ff77f117ee
Manuel Fahmy <manuelfahmy@gmail.com>
2020-05-29 00:52:50 +0200

    update

--- function callen.py
+++ function callen.py
@@ -1,8 +1,19 @@
 # function tests
 
+Zahl1 = ()
+Zahl2 = ()
+Zahl3 = ()
+
+print("Define two numbers")
+prompt = input()
+
+
 def add(a, b): 
-    c = a + b
-    return c
+    a = Zahl1
+    b = Zahl2
+    c = Zahl1 + Zahl2
+    c = Zahl3
+    return Zahl3
 
 def subs(a, b): 
     c = a - b
@@ -15,5 +26,9 @@ def mult(a, b):
 def divd(a, b): 
     c = a / b
 
-prompt = input()
 
+
+print("Choose what you want to do")
+choosing = input()
+if (choosing=="add"): 
+    print(Zahl3)
