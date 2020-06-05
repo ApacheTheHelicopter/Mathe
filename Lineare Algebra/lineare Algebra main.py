44474b3687b6b005f5489c73f32b7e3e59508766
Manuel Laptop <manuelfahmyz@gmail.com>
2020-06-05 11:57:52 +0200

    bug free since 1924

--- Lineare Algebra/lineare Algebra main.py
+++ Lineare Algebra/lineare Algebra main.py
@@ -39,52 +39,70 @@ print("Choose two Vectors. Default value is 1 ")
 print("First: ")
 
 Vektor1 = np.array([])
-n = int(input("Enter the dimensions you want:  "))
+n = input("Enter the dimensions you want:  ")
 
-for i in range(n):
-    v = input("Element:  ")
-    Vektor1 = np.append(Vektor1, float(v))
-print(Vektor1)
+n.isdigit()
 
-print("")
-print("Now the second vector: ")
-
-Vektor2 = np.array([])
-n = int(input("Enter the dimensions you want:  "))
-
-for i in range(n):
-    v = input("Element:  ")
-    Vektor2 = np.append(Vektor2, float(v))
-print(Vektor2)
-
-def VectorAdd(a, b):
-    c = np.add(a,b)
-    return c
-
-def VectorSubs(a, b): 
-    c = np.subtract(a,b)
-    return c
-
-def VectorMulti(a, b): 
-    c = np.multiply(a,b)
-    return c
-
-# wenn /0 nicht inf, sondern 0 stehen haben oder so.
-def VectorDivd(a, b): 
-    c = np.divide(a, b)
-    return c
-
-print("Choose what to do with the Vector!")
-print("You can choose from 'add', 'sub', 'mult' and 'div'")
-
-choose = input()
-
-if (choose == "add"):
-    print(VectorAdd(Vektor1, Vektor2))
-if (choose == "sub"): 
-    print(VectorSubs(Vektor1, Vektor2))
-if (choose == "div"): 
-    print(VectorDivd(Vektor1, Vektor2))
-if (choose == "mult"): 
-    print(VectorMulti(Vektor1, Vektor2))
+if (n.isdigit() == True):
+    n = int(n)
+    print("Ist digit!")
     
+    for i in range(n):
+        v = input("Element:  ")
+        Vektor1 = np.append(Vektor1, float(v))
+    print(Vektor1)
+
+    print("")
+    print("Now the second vector: ")
+
+    Vektor2 = np.array([])
+    n = input("Enter the dimensions you want:  ")
+
+    n.isdigit()
+
+    if (n.isdigit() == True):
+        n = int(n)
+        print("Ist digit!")
+
+        # Code
+        for i in range(n):
+            v = input("Element:  ")
+            Vektor2 = np.append(Vektor2, float(v))
+        print(Vektor2)
+
+        def VectorAdd(a, b):
+            c = np.add(a,b)
+            return c
+
+        def VectorSubs(a, b): 
+            c = np.subtract(a,b)
+            return c
+
+        def VectorMulti(a, b): 
+            c = np.multiply(a,b)
+            return c
+
+        # wenn /0 nicht inf, sondern 0 stehen haben oder so.
+        def VectorDivd(a, b): 
+            c = np.divide(a, b)
+            return c
+
+        print("Choose what to do with the Vector!")
+        print("You can choose from 'add', 'sub', 'mult' and 'div'")
+
+        choose = input()
+
+        if (choose == "add"):
+            print(VectorAdd(Vektor1, Vektor2))
+        if (choose == "sub"): 
+            print(VectorSubs(Vektor1, Vektor2))
+        if (choose == "div"): 
+            print(VectorDivd(Vektor1, Vektor2))
+        if (choose == "mult"): 
+            print(VectorMulti(Vektor1, Vektor2))
+        
+        else:
+            print("You took the wrong path, buddy. Enter a number next time.")
+            
+    else:
+        print("You took the wrong path, buddy. Enter a number next time.")
