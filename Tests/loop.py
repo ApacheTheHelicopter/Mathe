a95a4d467628903637ba2a2c289540be40b73e5f
Manuel Laptop <manuelfahmyz@gmail.com>
2020-08-28 10:59:52 +0200

    lol

--- Tests/loop.py
+++ Tests/loop.py
@@ -1,2 +1,15 @@
 # Loop test
 
+#List maker
+
+import numpy as np
+
+list = np.array([])
+
+n = input()
+
+for i in range(n):
+    v = input("Liste: ")
+    target = np.append(target, float(v))
+
+print (v)
