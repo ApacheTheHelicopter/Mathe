d459bb5e0dde1c6face3738098c81fcf76bec597
Manuel Laptop <manuelfahmyz@gmail.com>
2020-08-31 13:06:40 +0200

    update

--- Analysis/Funktionen.py
+++ Analysis/Funktionen.py
@@ -6,6 +6,13 @@ import numpy as np
 originalfunktion = np.array([])
 
 
+# Form des inputs: ax^n + bx^n + cx^n + d
+
+zahlenanzahl = input()
+
+for i in range(zahlenanzahl):
+    v = input("Dimensionen: " )
+    zahlenanzahl = np.append(zahlenanzahl, v)
 
 
 # Ableitungen berechen
